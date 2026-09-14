18d3adb9d8f4b3911c50ef7c64938aa53953fb7d
Le Bao Uyen Truong <lebaouyentruong@Salavi.local>
2026-09-14 11:20:25 +0930

    Return multiple recordings instead 1 recording

--- backend/src/main/java/com/sala/cassette/controller/RecordingController.java
+++ backend/src/main/java/com/sala/cassette/controller/RecordingController.java
@@ -1,22 +1,40 @@
 package com.sala.cassette.controller;
 
-import com.sala.cassette.model.Recording;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sala.cassette.model.Recording;
+
 @RestController
 @RequestMapping("/api/recordings")
-
 public class RecordingController {
 
-    @GetMapping
-    public Recording getRecording() {
+    private List<Recording> recordings = new ArrayList<>();
 
-        return new Recording(
-            1L,
-            "My First Cassette",
-            "This is a test transcript."
+    public RecordingController() {
+        recordings.add(
+            new Recording(
+                1L,
+                "Lecture",
+                "This is my first lecture transcript."
+            )
         );
+
+        recordings.add(
+            new Recording(
+                2L,
+                "Meeting",
+                "This is my meeting transcript."
+            )
+        );
+    }
+
+    @GetMapping
+    public List<Recording> getRecordings() {
+        return recordings;
     }
 }
