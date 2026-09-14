f8c11a9893ea83a891e565ec1befd49cc77c5e32
Le Bao Uyen Truong <lebaouyentruong@Salavi.local>
2026-09-14 11:34:41 +0930

    Updating Recording.java, add post to RecordingController to accept new recordings

--- backend/src/main/java/com/sala/cassette/controller/RecordingController.java
+++ backend/src/main/java/com/sala/cassette/controller/RecordingController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,8 @@ import com.sala.cassette.model.Recording;
 @RestController
 @RequestMapping("/api/recordings")
 public class RecordingController {
-
     private List<Recording> recordings = new ArrayList<>();
+    private long nextId = 3;
 
     public RecordingController() {
         recordings.add(
@@ -23,7 +25,6 @@ public class RecordingController {
                 "This is my first lecture transcript."
             )
         );
-
         recordings.add(
             new Recording(
                 2L,
@@ -32,9 +33,14 @@ public class RecordingController {
             )
         );
     }
-
     @GetMapping
     public List<Recording> getRecordings() {
         return recordings;
+    }
+    @PostMapping
+    public Recording addRecording(@RequestBody Recording recording) {
+        recording.setId(nextId++);
+        recordings.add(recording);
+        return recording;
     }
 }
--- backend/src/main/java/com/sala/cassette/model/Recording.java
+++ backend/src/main/java/com/sala/cassette/model/Recording.java
@@ -1,10 +1,13 @@
 package com.sala.cassette.model;
 
 public class Recording {
+
     private Long id;
     private String name;
     private String transcript;
 
+    // Empty constructor for Spring
+    public Recording() {}
     public Recording(Long id, String name, String transcript) {
         this.id = id;
         this.name = name;
@@ -18,5 +21,14 @@ public class Recording {
     }
     public String getTranscript() {
         return transcript;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setTranscript(String transcript) {
+        this.transcript = transcript;
     }
 }
